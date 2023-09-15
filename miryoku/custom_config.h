08edfca6b8ed0d612b9764228db677235b047ef9
CooperCarnahan <coopcarn@gmail.com>
2023-09-15 12:18:53 -0500

    Modify custom_config.h w/ defaults

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
-// Copyright 2021 Manna Harbour
+// Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_LAYERS_FLIP
